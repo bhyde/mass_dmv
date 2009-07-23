e7f494600e94a4520767eb3708be7c1d38933af9
Ben Hyde <bhyde@elm.cozy.org>
2009-07-23 13:38:51 -0400

    No output when office closed.

--- doit.rb
+++ doit.rb
@@ -3,7 +3,12 @@ require 'rubygems'
 require 'rest_client'
 require 'office'
 
+pat = /Licensing:\<\/div>The current wait time is:\<div class="time">(\d\d):(\d\d):(\d\d)/
 p = RestClient.get 'http://www.mass.gov/qrmv/' + Office::NAME[0..7] + '.shtm'
-/Licensing:\<\/div>The current wait time is:\<div class="time">(\d\d):(\d\d):(\d\d)/ =~ p
-m = 60* $1.to_i() + $2.to_i()
-RestClient.post 'http://rifgraf.heroku.com/graphs/dmv_' + Office::NAME, :value => m
+if pat  =~ p then
+  m = 60* $1.to_i() + $2.to_i()
+#  print m, "\n"
+  RestClient.post 'http://rifgraf.heroku.com/graphs/dmv_' + Office::NAME, :value => m
+else
+#  print "no match\n"
+end
